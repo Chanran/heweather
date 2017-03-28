71168d90e9f26bc37cb00f0d919edd6bfec59f40
blue <rong650751@163.com>
2017-03-28 18:27:08 +0800

    Layout组件

---
+++ src/components/Layout.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <x-header>当前天气</x-header>
+    <router-view></router-view>
+    <tabbar>
+      <tabbar-item link="/now">
+        <span slot="label">当前</span>
+      </tabbar-item>
+      <tabbar-item link="/hourly">
+        <span slot="label">小时</span>
+      </tabbar-item>
+      <tabbar-item link="/forecast">
+        <span slot="label">预报</span>
+      </tabbar-item>
+      <tabbar-item link="/suggestion">
+        <span slot="label">指数</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import { XHeader, Tabbar, TabbarItem } from 'vux'
+
+export default {
+  name: 'layout',
+  components: {
+    XHeader,
+    Tabbar,
+    TabbarItem
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Now from '@/components/Now' // 实时天气组件
+import Layout from '@/components/Layout'
 import Forecast from '@/components/Forecast' // 天气预报组件
-import Suggestion from '@/components/Suggestion' // 生活指数组件
 import Hourly from '@/components/Hourly' // 每小时天气预报组件
+import Now from '@/components/Now' // 实时天气组件
+import Suggestion from '@/components/Suggestion' // 生活指数组件
 
 // 使用router
 Vue.use(Router)
@@ -11,24 +12,30 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/now',
-      name: 'now',
-      component: Now
-    },
-    {
-      path: '/forecast',
-      name: 'Forecast',
-      component: Forecast
-    },
-    {
-      path: '/hourly',
-      name: 'Hourly',
-      component: Hourly
-    },
-    {
-      path: '/suggestion',
-      name: 'Suggestion',
-      component: Suggestion
+      path: '/',
+      component: Layout,
+      children: [
+        {
+          path: 'now',
+          name: 'now',
+          component: Now
+        },
+        {
+          path: 'forecast',
+          name: 'Forecast',
+          component: Forecast
+        },
+        {
+          path: 'hourly',
+          name: 'Hourly',
+          component: Hourly
+        },
+        {
+          path: 'suggestion',
+          name: 'Suggestion',
+          component: Suggestion
+        }
+      ]
     }
   ]
 })
